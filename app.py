c9c4c9058fc1625ca643ff4e88c18564805ce46f
Chirag <76880977+chirag127@users.noreply.github.com>
2023-10-01 18:20:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,21 @@ import sqlite3
 import re
 
 # Establish a connection to the SQLite database
-conn = sqlite3.connect("error_database.db")
-cursor = conn.cursor()
+@st.cache(allow_output_mutation=True)
+def get_database_connection():
+    conn = sqlite3.connect("error_database.db")
+    return conn
+
+@st.cache(allow_output_mutation=True)
+def get_cursor(conn):
+    return conn.cursor()
+
+# Streamlit app
+st.title("Error Handler")
+
+# Get the database connection and cursor
+conn = get_database_connection()
+cursor = get_cursor(conn)
 
 # Create a table if it doesn't exist
 cursor.execute('''
@@ -19,6 +32,7 @@ cursor.execute('''
 conn.commit()
 
 # Function to check if the entered error matches any regex
+@st.cache
 def check_error(error_text):
     cursor.execute("SELECT * FROM errors")
     rows = cursor.fetchall()
@@ -26,9 +40,6 @@ def check_error(error_text):
         if re.search(row[2], error_text):
             return (True, row[3], row[4])
     return (False, None, None)
-
-# Streamlit app
-st.title("Error Handler")
 
 # User input for error
 user_error = st.text_area("Enter your error message:")
@@ -44,12 +55,13 @@ if st.button("Check Error"):
         st.write("You can add this error to the database below:")
 
         # Allow the user to add the error to the database
-        new_error_name = st.text_input("Error Name:")
-        new_error_regex = st.text_input("Error Regex (in a more generalized form):")
-        new_error_reason = st.text_area("Error Reason:")
-        new_error_solution = st.text_area("Error Solution:")
+        new_error_name = st.text_input("Error Name:", key="error_name")
+        new_error_regex = st.text_input("Error Regex (in a more generalized form):", key="error_regex")
+        new_error_reason = st.text_area("Error Reason:", key="error_reason")
+        new_error_solution = st.text_area("Error Solution:", key="error_solution")
 
         if st.button("Add Error to Database"):
+            cursor = get_cursor(conn)  # Get a new cursor (important for SQLite)
             cursor.execute("INSERT INTO errors (name, regex, reason, solution) VALUES (?, ?, ?, ?)",
                            (new_error_name, new_error_regex, new_error_reason, new_error_solution))
             conn.commit()
